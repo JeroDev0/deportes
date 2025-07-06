99ae3fd8db29c0217380dce4fa7d42c9e3a62640
John <jerodev0@gmail.com>
2025-07-06 16:37:36 -0500

    pruueba 2 nuevo perfil

--- deportes-frontend/src/pages/ProfilePage.jsx
+++ deportes-frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,6 @@
 import ProfileSidebar from "../components/Profile/ProfileSidebar";
 import ProfileMain from "../components/Profile/ProfileMain";
-import ProfileTabs from "../components/Profile/ProfileTabs";
-import ProfileRightbar from "../components/Profile/SidebarProfiles";
+import SidebarProfiles from "../components/Profile/SidebarProfiles";
 import styles from "./ProfilePage.module.css";
 
 function ProfilePage() {
@@ -11,12 +10,8 @@ function ProfilePage() {
         <ProfileSidebar />
       </aside>
       <main className={styles.center}>
-        <div className={styles.mainTop}>
-          <ProfileMain />
-        </div>
-        {/* <div className={styles.mainBottom}>
-          <ProfileTabs />
-        </div> */}
+        <ProfileMain />
+        {/* Aquí después puedes poner el módulo de pestañas */}
       </main>
       <aside className={styles.right}>
         <SidebarProfiles />
