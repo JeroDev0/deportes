53442dfabc25d3a64a31e8d9c8d1ac5cfdd91748
John <jerodev0@gmail.com>
2025-07-06 16:48:51 -0500

    Actualiza estructura de perfil a 3 columnas 2

--- deportes-frontend/src/pages/ProfilePage.jsx
+++ deportes-frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import ProfileSidebar from "../components/Profile/ProfileSidebar";
 import ProfileMain from "../components/Profile/ProfileMain";
 import SidebarProfiles from "../components/Profile/SidebarProfiles";
-import styles from "./ProfilePage.module.css";
+import styles from "./ProfilePage.module.css"; 
 
 function ProfilePage() {
   return (
